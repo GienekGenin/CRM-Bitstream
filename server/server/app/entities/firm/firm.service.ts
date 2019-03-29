9f00e96d56182e97ec714e98a1949a962dd2b6aa
GienekGenin <genin.gennadiigendos2010@yandex.ru>
2019-03-29 15:36:13 +0100

    Server update & delete firm error handler

--- server/server/app/entities/firm/firm.service.ts
+++ server/server/app/entities/firm/firm.service.ts
@@ -2,31 +2,49 @@ import {FirmRepository} from './firm.repository';
 import {Types} from 'mongoose';
 
 class FirmService {
-	private firmRepository: FirmRepository;
+    private firmRepository: FirmRepository;
 
-	constructor() {
-		this.firmRepository = new FirmRepository();
-	}
+    constructor() {
+        this.firmRepository = new FirmRepository();
+    }
 
-	getAll() {
-		return this.firmRepository.getAll();
-	}
+    getAll() {
+        return this.firmRepository.getAll();
+    }
 
-	findById(id: Types.ObjectId) {
-		return this.firmRepository.findById(id);
-	}
+    findById(id: Types.ObjectId) {
+        return this.firmRepository.findById(id);
+    }
 
-	removeById(id){
-		return this.firmRepository.removeById(id);
-	}
+    removeById(id) {
+        return new Promise(((resolve, reject) => {
+            this.firmRepository.removeById(id)
+                .then(d => {
+                    if (d['deletedCount'] === 0) {
+                        reject(new Error('Unable to delete'));
+                    }
+                    resolve(d);
+                })
+                .catch(e => reject(e));
+        }))
+    }
 
-	updateById(id,obj){
-		return this.firmRepository.update(id, obj);
-	}
+    updateById(id, obj) {
+        return new Promise(((resolve, reject) => {
+            this.firmRepository.update(id, obj)
+                .then(d => {
+                    if (d['nModified'] === 0) {
+                        reject(new Error('Unable to update'));
+                    }
+                    resolve(d);
+                })
+                .catch(e => reject(e));
+        }))
+    }
 
-	save(firm) {
-		return this.firmRepository.save(firm);
-	}
+    save(firm) {
+        return this.firmRepository.save(firm);
+    }
 }
 
 export const firmService = new FirmService();
