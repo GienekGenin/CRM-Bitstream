774112080f41ceaf49b6e8a227cab59edf4152f3
GienekGenin <genin.gennadiigendos2010@yandex.ru>
2019-04-01 10:34:38 +0200

    Basic redux structure for users

---
+++ client/src/redux/actions/users.js
@@ -0,0 +1,25 @@
+import {userConstants} from "../constants/index";
+
+export const usersRequest = () => {
+    return {type: userConstants.USERS_GET_REQUEST}
+};
+
+export const cleanUsersErrors = () => {
+    return {type: userConstants.CLEAN_USERS_ERRORS}
+};
+
+export const cleanUsersSuccess = () => {
+    return {type: userConstants.CLEAN_USERS_SUCCESS}
+};
+
+export const updateFirmRequest = (user) => {
+    return {type: userConstants.UPDATE_USER_REQUEST, payload: user}
+};
+
+export const addUserRequest = (payload) => {
+    return {type: userConstants.ADD_USER_REQUEST, payload}
+};
+
+export const deleteUserRequest = (userEmail) => {
+    return {type: userConstants.DELETE_USER_REQUEST, payload: userEmail}
+};
---
+++ client/src/redux/reducers/users.js
@@ -0,0 +1,78 @@
+import {userConstants} from "../constants/index";
+
+const initialState = {
+    users: null,
+    loading: false,
+    error: null,
+    success: null
+};
+
+export const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case userConstants.USERS_GET_REQUEST:
+        case userConstants.ADD_USER_REQUEST:
+        case userConstants.UPDATE_USER_REQUEST:
+        case userConstants.DELETE_USER_REQUEST: {
+            return {
+                ...state,
+                loading: true
+            }
+        }
+        case userConstants.ADD_USER_SUCCESS: {
+            return {
+                ...state,
+                users: [...state.users, action.payload.user],
+                loading: false,
+                success: action.payload.success
+            }
+        }
+        case userConstants.UPDATE_USER_SUCCESS:{
+            return {
+                ...state,
+                users: state.users.map(el => (el._id === action.payload.user._id) ? action.payload.user : el),
+                loading: false,
+                success: action.payload.success
+            }
+        }
+        case userConstants.DELETE_USER_SUCCESS: {
+            return {
+                users: state.users.filter(el => el._id !== action.payload),
+                loading: false,
+                success: 'User was successfully deleted'
+            }
+        }
+        case userConstants.USERS_GET_SUCCESS: {
+            return Object.assign({}, state, {
+                users: action.payload,
+                loading: false,
+                success: 'Users loaded successfully'
+            });
+        }
+        case userConstants.USERS_GET_FAILURE:
+        case userConstants.UPDATE_USER_FAILURE:
+        case userConstants.ADD_USER_FAILURE:
+        case userConstants.DELETE_USER_FAILURE: {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        }
+        case userConstants.CLEAN_USERS_ERRORS: {
+            return {
+                ...state,
+                error: null
+            }
+        }
+        case userConstants.CLEAN_USERS_SUCCESS: {
+            return {
+                ...state,
+                success: null
+            }
+        }
+        default:
+            return state;
+    }
+};
+
